84a0f945b53f42549f36dba8c4785dc4691ad9da
izsuchit <84307350+izsuchit@users.noreply.github.com>
2022-07-03 23:34:45 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const client =new Client({
     host : "localhost",
     user : "postgres",
     port :5432,
-    password :"suchit",
+    password :"*******",  // Replace it with your postgreySQl account password
     database :"postgres"
 })
 module. exports = client;
